03a86af0c0858ac687fc5c5a61c21d338ffa41e5
codeblender <codeblender@gmail.com>
2024-03-31 08:51:32 +0900

    splash window +

--- WpfMvvmApp/SplashWindow.xaml.cs
+++ WpfMvvmApp/SplashWindow.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Shapes;
 
 namespace WpfMvvmApp
 {
-    /// <summary>
-    /// SplashWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class SplashWindow : Window
     {
         public SplashWindow()
