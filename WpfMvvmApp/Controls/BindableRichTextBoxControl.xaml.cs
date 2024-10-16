eea896f8b4950afb19afbf9de9773007c1d98f3f
김호섭 <hskim@beaverworksinc.com>
2024-10-16 18:55:37 +0900

    richtextbox binding

---
+++ WpfMvvmApp/Controls/BindableRichTextBoxControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace WpfMvvmApp.Controls
+{
+    public partial class BindableRichTextBoxControl : UserControl
+    {
+        public BindableRichTextBoxControl()
+        {
+            InitializeComponent();
+
+            this.TextBox.TextChanged += TextBox_TextChanged;
+            this.RichTextBox.TextChanged += RichTextBox_TextChanged;
+        }
+
+        public static readonly DependencyProperty DocumentStringProperty =
+            DependencyProperty.Register(
+                nameof(DocumentString),
+                typeof(string),
+                typeof(BindableRichTextBoxControl),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string DocumentString
+        {
+            get => (string)GetValue(DocumentStringProperty);
+            set => SetValue(DocumentStringProperty, value);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.TextBox.Text))
+                return;
+
+            this.RichTextBox.TextChanged -= RichTextBox_TextChanged;
+            
+            this.RichTextBox.Document = (FlowDocument)XamlReader.Parse(this.TextBox.Text);
+
+            this.RichTextBox.TextChanged += RichTextBox_TextChanged;
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.TextBox.TextChanged -= TextBox_TextChanged;
+
+            this.TextBox.Text = XamlWriter.Save(this.RichTextBox.Document);
+
+            this.TextBox.TextChanged += TextBox_TextChanged;
+        }
+    }
+}
