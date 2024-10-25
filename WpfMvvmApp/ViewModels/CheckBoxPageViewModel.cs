da3169939e18b96d4374e426bc4aa28b3e3cfef1
김호섭 <hskim@beaverworksinc.com>
2024-10-25 11:04:51 +0900

    checkbox page

--- WpfMvvmApp/ViewModels/CheckBoxPageViewModel.cs
+++ WpfMvvmApp/ViewModels/CheckBoxPageViewModel.cs
@@ -6,24 +6,8 @@ namespace WpfMvvmApp.ViewModels
 {
     public partial class CheckBoxPageViewModel : ViewModelBase
     {
-        [ObservableProperty]
-        private string _radioValue;
-
         public CheckBoxPageViewModel()
         {
-            this.RadioValue = "1";
-        }
-
-        [RelayCommand]
-        private void Select(object value)
-        {
-            this.RadioValue = value as string;
-        }
-
-        [RelayCommand]
-        private void Show()
-        {
-            MessageBox.Show(this.RadioValue);
         }
     }
 }
