faef1dd5f1b3a5100472540ce250ecbab21481d9
codeblender <codeblender@gmail.com>
2024-10-27 10:25:50 +0900

    binding page arrange

--- WpfMvvmApp/ViewModels/ItemsBindingPageViewModel.cs
+++ WpfMvvmApp/ViewModels/ItemsBindingPageViewModel.cs
@@ -12,12 +12,6 @@ namespace WpfMvvmApp.ViewModels
         [ObservableProperty]
         private ObservableCollection<SampleDataListItemModel> _sampleDataList;
 
-        [ObservableProperty]
-        private ObservableCollection<SampleDataListItem2ViewModel> _sampleDataList2;
-
-        [ObservableProperty]
-        private ObservableCollection<SampleDataListItemModel> _sampleDataList3;
-
         public ItemsBindingPageViewModel()
         {
             this.SampleDataList = new ObservableCollection<SampleDataListItemModel>();
@@ -27,22 +21,6 @@ namespace WpfMvvmApp.ViewModels
             this.SampleDataList.Add(new SampleDataListItemModel { Id = 4, Text = "Item4", Selected = false, Color = SampleDataListItemColorEnum.Red });
             this.SampleDataList.Add(new SampleDataListItemModel { Id = 5, Text = "Item5", Selected = true, Color = SampleDataListItemColorEnum.White });
             this.SampleDataList.Add(new SampleDataListItemModel { Id = 6, Text = "Item6", Selected = false, Color = SampleDataListItemColorEnum.Blue });
-
-            this.SampleDataList2 = new ObservableCollection<SampleDataListItem2ViewModel>();
-            this.SampleDataList2.Add(new SampleDataListItem2ViewModel { Id = 1, Text = "Item1", Selected = false, Color = SampleDataListItemColorEnum.Red });
-            this.SampleDataList2.Add(new SampleDataListItem2ViewModel { Id = 2, Text = "Item2", Selected = true, Color = SampleDataListItemColorEnum.Red });
-            this.SampleDataList2.Add(new SampleDataListItem2ViewModel { Id = 3, Text = "Item3", Selected = false, Color = SampleDataListItemColorEnum.Blue });
-            this.SampleDataList2.Add(new SampleDataListItem2ViewModel { Id = 4, Text = "Item4", Selected = false, Color = SampleDataListItemColorEnum.Red });
-            this.SampleDataList2.Add(new SampleDataListItem2ViewModel { Id = 5, Text = "Item5", Selected = true, Color = SampleDataListItemColorEnum.White });
-            this.SampleDataList2.Add(new SampleDataListItem2ViewModel { Id = 6, Text = "Item6", Selected = false, Color = SampleDataListItemColorEnum.Blue });
-
-            this.SampleDataList3 = new ObservableCollection<SampleDataListItemModel>();
-            this.SampleDataList3.Add(new SampleDataListItemModel { Id = 1, Text = "Item1", Selected = false, Color = SampleDataListItemColorEnum.Red });
-            this.SampleDataList3.Add(new SampleDataListItemModel { Id = 2, Text = "Item2", Selected = true, Color = SampleDataListItemColorEnum.Red });
-            this.SampleDataList3.Add(new SampleDataListItemModel { Id = 3, Text = "Item3", Selected = false, Color = SampleDataListItemColorEnum.Blue });
-            this.SampleDataList3.Add(new SampleDataListItemModel { Id = 4, Text = "Item4", Selected = false, Color = SampleDataListItemColorEnum.Red });
-            this.SampleDataList3.Add(new SampleDataListItemModel { Id = 5, Text = "Item5", Selected = true, Color = SampleDataListItemColorEnum.White });
-            this.SampleDataList3.Add(new SampleDataListItemModel { Id = 6, Text = "Item6", Selected = false, Color = SampleDataListItemColorEnum.Blue });
         }
 
         [RelayCommand]
