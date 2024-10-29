6b55f2e5eabcf4756471a7bcd57a3a69d5869722
김호섭 <hskim@beaverworksinc.com>
2024-10-29 13:26:39 +0900

    dialog : timespan -> token

--- WpfMvvmApp/Bases/DialogBase.cs
+++ WpfMvvmApp/Bases/DialogBase.cs
@@ -6,15 +6,17 @@ namespace WpfMvvmApp.Dialogs
     public class DialogBase : UserControl
     {
         private TaskCompletionSource<bool> _taskCompletionSource;
+        private CancellationToken _cancellationToken;
 
         public DialogBase()
         {
             _taskCompletionSource = new TaskCompletionSource<bool>();
+            _cancellationToken = new CancellationToken();
         }
 
         public async Task<bool> WaitAsync()
         {
-            return await _taskCompletionSource.Task.WaitAsync(Timeout.InfiniteTimeSpan);
+            return await _taskCompletionSource.Task.WaitAsync(_cancellationToken);
         }
 
         public void Close(bool result)
